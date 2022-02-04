1181c2db5920489210e4a0006c04fa5e3e35dcd6
StasyMo <anmolodtsova@mail.ru>
2022-02-04 17:54:13 +0300

    fixed linter error

--- gendiff/tests/test_file1_file2.py
+++ gendiff/tests/test_file1_file2.py
@@ -1,14 +1,16 @@
 from gendiff.scripts.gendiff import generate_diff
 
+
 def main():
     file1_path = 'gendiff/tests/fixtures/file1.json'
     file2_path = 'gendiff/tests/fixtures/file2.json'
-    file_1_file2 = open('gendiff/tests/fixtures/correct_answer_file1_first.json')
-    equal_files = open('gendiff/tests/fixtures/correct_answer_equal.json')
-    file_2_file1 = open('gendiff/tests/fixtures/correct_answer_file2_first.json')
+    file_1_file2 = open('gendiff/tests/fixtures/corr_answer_file1_first.json')
+    equal_files = open('gendiff/tests/fixtures/corr_answer_equal.json')
+    file_2_file1 = open('gendiff/tests/fixtures/corr_answer_file2_first.json')
     assert generate_diff(file1_path, file2_path) == file_1_file2.read()
     assert generate_diff(file1_path, file1_path) == equal_files.read()
     assert generate_diff(file2_path, file1_path) == file_2_file1.read()
+
 
 if __name__ == '__main__':
     main()
