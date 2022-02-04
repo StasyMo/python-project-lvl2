0efcfe04df3ae4e63e361d97fa9e201a3c4763a6
StasyMo <anmolodtsova@mail.ru>
2022-02-04 17:44:54 +0300

    457

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -16,24 +16,24 @@ def generate_diff(first_file, second_file):
     second_file = json.load(open(second_file))
     sorted_dict_keys_1 = sorted(first_file.keys())
     sorted_dict_keys_2 = sorted(second_file.keys())
-    first_file_dict = {}
-    second_file_dict = {}
+    file1_dict = {}
+    file2_dict = {}
     for item in sorted_dict_keys_1:
         if first_file.get(item) == second_file.get(item):
-            first_file_dict[item] = ' '
+            file1_dict[item] = ' '
         else:
-            first_file_dict[item] = '-'
+            file1_dict[item] = '-'
     for item in sorted_dict_keys_2:
         if first_file.get(item) != second_file.get(item):
-            second_file_dict[item] = '+'
-    diff_list_1 = [make_list_of_diff(first_file_dict[item], item, first_file[item])
-                   for item in first_file_dict]
-    diff_list_2 = [make_list_of_diff(second_file_dict[item], item, second_file[item])
-                   for item in second_file_dict]
-    return print('\n'.join(diff_list_1 + diff_list_2))
+            file2_dict[item] = '+'
+    diff_list_1 = [make_diff_list(file1_dict[item], item, first_file[item])
+                   for item in file1_dict]
+    diff_list_2 = [make_diff_list(file2_dict[item], item, second_file[item])
+                   for item in file2_dict]
+    return '\n'.join(diff_list_1 + diff_list_2)
 
 
-def make_list_of_diff(sign, item, value):
+def make_diff_list(sign, item, value):
     return '{} {}: {}'.format(sign, item, str(value).lower())
 
 
